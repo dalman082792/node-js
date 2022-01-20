4d32bd33425a5cbc217e62970b055a70f1e15691
David Alemán Sánchez <david082792@gmail.com>
2022-01-19 18:08:57 -0600

    actualizacion del proyecto hasta el manejo de post

--- curso/app.js
+++ curso/app.js
@@ -1,1 +1,34 @@
 const express = require("express");
+const app = express();
+app.use(express.json())
+
+const usuarios = [
+    {id:1, nombre:"miztli"},
+    {id:2, nombre:"nancy"},
+    {id:3, nombre:"david"}
+];
+
+app.get("/",(req, res)=>{
+    res.send("hola mundo con express")
+});
+app.get("/usuarios",(req, res)=>{
+    res.send(usuarios)
+});
+app.get("/usuarios/:id",(req, res)=>{
+    let usuario=usuarios.find(u=> u.id === parseInt(req.params.id))
+    if(!usuario) res.status(404).send("usuario no encontrado")
+    res.send(usuario)
+});
+app.post("/usuarios",(req, res)=>{
+    const usuario ={
+        id: usuarios.length + 1,
+        nombre: req.body.nombre
+    }
+    usuario.push(usuario)
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () =>{
+    console.log(`escuchando desde el puerto ${port}`);
+})
