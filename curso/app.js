71abb5c201b1761abe2ca8fc15ead81ce1608e46
David Alemán Sánchez <david082792@gmail.com>
2022-01-24 16:54:51 -0600

    se agregan los metodos put y delete se mejora el codigo convirtiendo algunas parted del codigo en funciones para ahorrar codigo, se agregan logs y variables de entorno para los ambientes, tambien se activa el modo debug para debugear las pruebas en diferentes ambientes

--- curso/app.js
+++ curso/app.js
@@ -1,8 +1,28 @@
+const debug = require("debug")("app:inicio");
+//const debugdb = require("debug")("app:db");
 const express = require("express");
 const Joi = require('joi');
+//const logger = require("./logger");
+const config = require("config")
+const morgan = require("morgan")
 const app = express();
-app.use(express.json())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+//app.use(logger);
+//configuracion de entornos o ambientes
+console.log("aplicacion: " + config.get("nombre"));
+console.log("base de datos: " + config.get("configdb.host"));
 
+//usando midelware de terceros nota: en la paguina de express podemos encontrar los midelwere de terceros
+if (app.get("env") === "develpmen") {
+    app.use(morgan("dev"));
+    //console.log("morgan ablitado..")
+    debug("morgan esta abilitado..")
+}
+
+//trabajando con la db
+debug("conectando a la bd")
 const usuarios = [
     { id: 1, nombre: "miztli" },
     { id: 2, nombre: "nancy" },
@@ -17,7 +37,10 @@ app.get("/usuarios", (req, res) => {
 });
 app.get("/usuarios/:id", (req, res) => {
     let usuario = encontrarUsuario(req.params.id)
-    if (!usuario) res.status(404).send("usuario no encontrado")
+    if (!usuario) {
+        res.status(404).send("usuario no encontrado")
+        return
+    }
     res.send(usuario)
 });
 app.post("/usuarios", (req, res) => {
@@ -39,9 +62,12 @@ app.post("/usuarios", (req, res) => {
         return
     }
 });
-app.put("/usuarios/:id",(req, res)=>{
+app.put("/usuarios/:id", (req, res) => {
     let usuario = encontrarUsuario(req.params.id)
-    if (!usuario) res.status(404).send("usuario no encontrado")
+    if (!usuario) {
+        res.status(404).send("usuario no encontrado")
+        return
+    }
     const { error, value } = validarUsuario(req.body.nombre)
     if (error) {
         const mensagge = error.details[0].message
@@ -52,19 +78,31 @@ app.put("/usuarios/:id",(req, res)=>{
     res.send(usuario)
 })
 
+app.delete("/usuarios/:id", (req, res) => {
+    let usuario = encontrarUsuario(req.params.id)
+    if (!usuario) {
+        res.status(404).send("usuario no encontrado")
+        return
+    }
+    const index = usuarios.indexOf(usuario)
+    usuarios.splice(index, 1)
+    res.send(usuarios)
+})
+
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log(`escuchando desde el puerto ${port}`);
 })
 
-function encontrarUsuario(id){
+function encontrarUsuario(id) {
     return usuarios.find(u => u.id === parseInt(id))
 }
 
-function validarUsuario(nombre){
+function validarUsuario(nombre) {
     const schema = Joi.object({
         nombre: Joi.string().min(3).max(10).required(),
     })
-    return schema.validate({ nombre:nombre });
+    return schema.validate({ nombre: nombre });
 }
